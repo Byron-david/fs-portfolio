ef44860d4b68c913febe88fa0dc86013e4779f33
Byron <byrondavid87@gmail.com>
2025-12-10 14:41:39 -0800

    Intro text added and formatted

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,23 +5,33 @@ import Section from './Section'
 
 function Intro() {
   return (
-    <Section >
+    <Section className={styles.introContainer}>
         {/* <div className={styles.roundedTag}>
           Full Stack Developer
         </div> */}
-        {/* <h2>
-          Full Stack Developer
-        </h2> */}
+
         <h1>
             Hello, I'm Byron David.
         </h1>
-
+        <h2>
+          Full Stack Developer
+        </h2>
+        {/* <span className={styles.highlight}>Full Stack Developer.</span> */}
         <p>
-          Technical Artist turned <span className={styles.highlight}>Full Stack Developer.</span> I build robust
-          web tools with the eye of a designer and the logic of a computer scientist.
+          I spent the last five years building pipelines for robotics at DeepMind and designed for
+          AAA games at PlayStation. Now, I’m bringing that same performance-obsessed mindset to the browser.
         </p>
-      <Button text="Portfolio" href="#portfolio"/>
-      <Button text="Resume" className={buttonStyles.buttonBorder}/>
+        <p>
+          I write code that creates experiences. Whether it's
+          Python automation or a React frontend, I build tools that are as satisfying to use as
+          they are to look at.
+        </p>
+
+      <div className={buttonStyles.buttonContainer}>
+        <Button text="Portfolio" href="#portfolio"/>
+        <Button text="Resume" className={buttonStyles.buttonBorder}/>
+      </div>
+
     </Section>
 
   );
